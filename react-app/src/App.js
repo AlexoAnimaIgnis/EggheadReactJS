998c014d9946ad3dafa7e5d59ad907d663ae9f1d
unknown <ballester.alexander@gmail.com>
2020-07-01 03:51:32 +0800

    display output in react in render method

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-// class App extends React.Component {
-//   render() {
-//     // return React.createElement('h1', null, 'Hello World Alexo')
-//     return <h1>Hello Eggheads</h1>
-//   }
-// }
-
-const App = () => <h1>Hello stateless</h1>
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+    <h1>Hello Eggheads</h1> <h3>BOLD</h3>
+      </div>
+    )
+  }
+}
 
 export default App
