4ab06a70db8d4b1eb9b153484ac4911677765c33
unknown <ballester.alexander@gmail.com>
2020-06-30 15:49:55 +0800

    write a hello world component

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
-const App = () => <h1>Hi Alexo</h1>
+// class App extends React.Component {
+//   render() {
+//     // return React.createElement('h1', null, 'Hello World Alexo')
+//     return <h1>Hello Eggheads</h1>
+//   }
+// }
+
+const App = () => <h1>Hello stateless</h1>
 
 export default App
